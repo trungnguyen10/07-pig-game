cf543bbc118cf676001afe157842f8bd206bea8f
Trung Nguyen <trungnguyen2809@gmail.com>
2022-01-16 09:27:22 -0600

    fix bug for resetGame() improve switchPlayer() to be more generic

--- script.js
+++ script.js
@@ -8,13 +8,19 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const currScoreEl = document.querySelector('.current-score');
+const currScoreEls = document.querySelectorAll('.current-score');
 
 // function to set game to starting conditions
 function resetGame() {
   for (let element of scoreEls) element.textContent = 0;
   diceEl.classList.add('hidden');
-  currScoreEl.textContent = 0;
+  for (let element of currScoreEls) element.textContent = 0;
+
+  // reset active player to player--0
+  let i = 0;
+  document.querySelector(`.player--${i++}`).classList.add('player--active');
+  for (; i < numberOfPlayer; i++)
+    document.querySelector(`.player--${i}`).classList.remove('player--active');
 }
 
 resetGame();
@@ -22,7 +28,7 @@ resetGame();
 // "roll a die" function return the rolled number (from 1 to 6)
 function rollADie() {
   const randomNumber = Math.trunc(Math.random() * 6 + 1);
-  console.log(randomNumber);
+  // console.log(`rolled number ${randomNumber}`);
 
   //display the die image corresponding to the rolled number
   let text = 'dice-' + randomNumber + '.png';
@@ -34,10 +40,10 @@ function rollADie() {
 
 // Get the active player number return 0 for 1st player and 1 for 2nd player
 function getActivePlayerNumber() {
-  const playersEl = document.querySelectorAll('.player');
+  const playerEls = document.querySelectorAll('.player');
   let i = 0;
-  for (; i < playersEl.length; i++) {
-    if (playersEl[i].classList.contains('player--active')) break;
+  for (; i < playerEls.length; i++) {
+    if (playerEls[i].classList.contains('player--active')) break;
   }
   return i;
 }
@@ -50,8 +56,9 @@ function switchPlayer() {
   activePlayerEl.classList.remove('player--active');
   activePlayerEl.querySelector('.current-score').textContent = 0;
 
+  let nextActivePlayerNumber = (i + 1) % numberOfPlayer;
   let otherPlayer = document.querySelector(
-    `.player--${numberOfPlayer - 1 - i}`
+    `.player--${nextActivePlayerNumber}`
   );
   otherPlayer.classList.add('player--active');
 }
@@ -63,7 +70,7 @@ function addToCurrentScore(rolledNumber) {
   let currentScore = Number(
     activePlayerEl.querySelector('.current-score').textContent
   );
-  console.log(activePlayerEl.querySelector('.current-score').textContent);
+  // console.log(`current score ${activePlayerEl.querySelector('.current-score').textContent}`);
   activePlayerEl.querySelector('.current-score').textContent =
     currentScore + rolledNumber;
 }
